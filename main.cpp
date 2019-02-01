bb6a816cbbceadf6ea448614153d53006dadaa7e
Noah1332 <47231182+Noah1332@users.noreply.github.com>
2019-01-31 20:04:39 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World Class
+//
+//  Created by Noah McGhghy on 1/28/19.
+//  Copyright © 2019 Noah McGhghy. All rights reserved.
+//
+
+#include <iostream>
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
